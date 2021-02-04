fd11a9be5f87ac4bef39137ed4be275fcecd0ae4
Nikita Tulenev <n1tulenev@gmail.com>
2021-02-04 17:28:45 +0300

    Add topic name to topic offsets object

--- src/KafkaAsTable/Model/TopicOffsets.cs
+++ src/KafkaAsTable/Model/TopicOffsets.cs
@@ -2,10 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+using KafkaAsTable.Helpers;
+
 namespace KafkaAsTable.Model
 {
     public class TopicOffsets
     {
+        private readonly string _topicName;
+
+        public TopicOffsets(string topicName)
+        {
+            KafkaValidationHelper.ValidateTopicName(topicName);
+
+            _topicName = topicName;
+        }
+
         // TODO Add topic name, offset and consumer integration logic
 
         // All offsets complicated logic should be moved in this class
