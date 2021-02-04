bcd8377d9a033edf63d66fcb9d2c78439b5b146a
Nikita Tulenev <n1tulenev@gmail.com>
2021-02-04 18:30:28 +0300

    Add PartitionWatermark class

---
+++ src/KafkaAsTable/Model/PartitionWatermark.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Confluent.Kafka;
+
+namespace KafkaAsTable.Model
+{
+    public class PartitionWatermark
+    {
+        //public PartitionWatermark(string topic,)
+
+        //private readonly Partition _partition;
+
+        //private readonly WatermarkOffsets _offset;
+
+        //private readonly string _topicName;
+    }
+}
