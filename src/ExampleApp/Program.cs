9ce2a5687a8e1bcc18dae37b485b458003ed0a91
Nikita Tulenev <n1tulenev@gmail.com>
2021-02-06 07:41:11 +0300

    Add exampleApp project

---
+++ src/ExampleApp/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ExampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
