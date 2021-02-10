9fa7cad1e8c300e9d3ce9d8da6d9c1fe3d21a78d
Nikita Tulenev <n1tulenev@gmail.com>
2021-02-10 09:10:02 +0300

    Add event args doc

--- src/KafkaAsTable/Events/KafkaInitTableArgs{Key,Value}.cs
+++ src/KafkaAsTable/Events/KafkaInitTableArgs{Key,Value}.cs
@@ -3,10 +3,22 @@ using System.Collections.Immutable;
 
 namespace KafkaAsTable.Events
 {
+    /// <summary>
+    /// <see cref="EventArgs"/> for Init Table event.
+    /// </summary>
+    /// <typeparam name="Key">Message key.</typeparam>
+    /// <typeparam name="Value">Message value.</typeparam>
     public class KafkaInitTableArgs<Key, Value> : EventArgs where Key : notnull
     {
+        /// <summary>
+        /// Table state.
+        /// </summary>
         public ImmutableDictionary<Key, Value> State { get; }
 
+        /// <summary>
+        /// Creates <see cref="KafkaInitTableArgs{Key, Value}"/>.
+        /// </summary>
+        /// <param name="state">Table state.</param>
         public KafkaInitTableArgs(ImmutableDictionary<Key, Value> state)
         {
             State = state ?? throw new ArgumentNullException(nameof(state));
--- src/KafkaAsTable/Events/KafkaUpdateTableArgs{Key,Value}.cs
+++ src/KafkaAsTable/Events/KafkaUpdateTableArgs{Key,Value}.cs
@@ -3,12 +3,28 @@ using System.Collections.Immutable;
 
 namespace KafkaAsTable.Events
 {
+    /// <summary>
+    /// <see cref="EventArgs"/> for update table event.
+    /// </summary>
+    /// <typeparam name="Key">Message key.</typeparam>
+    /// <typeparam name="Value">Message value.</typeparam>
     public class KafkaUpdateTableArgs<Key, Value> : EventArgs where Key : notnull
     {
+        /// <summary>
+        /// Table state.
+        /// </summary>
         public ImmutableDictionary<Key, Value> State { get; }
 
+        /// <summary>
+        /// Updated key.
+        /// </summary>
         public Key UpdatedKey { get; }
 
+        /// <summary>
+        /// Creates <see cref="KafkaUpdateTableArgs{Key, Value}"/>.
+        /// </summary>
+        /// <param name="state">Table state.</param>
+        /// <param name="key">Updated key.</param>
         public KafkaUpdateTableArgs(ImmutableDictionary<Key, Value> state, Key key)
         {
             State = state ?? throw new ArgumentNullException(nameof(state));
