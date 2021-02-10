6d47ee15f772f29699edd22469af15906967de49
Nikita Tulenev <n1tulenev@gmail.com>
2021-02-10 21:20:52 +0300

    Add KafkaInitTableArgsTests

---
+++ src/KafkaAsTable.Tests/KafkaInitTableArgs{Key,Value}.Tests.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Immutable;
+
+using FluentAssertions;
+
+using KafkaAsTable.Events;
+
+using Xunit;
+
+namespace KafkaAsTable.Tests
+{
+    public class KafkaInitTableArgsTests
+    {
+        [Fact(DisplayName = "KafkaInitTableArgs can't be created with null state.")]
+        [Trait("Category", "Unit")]
+        public void CantCreateKafkaInitTableArgsWithNullState()
+        {
+            // Arrange
+            ImmutableDictionary<object, object> state = null!;
+
+            // Act
+            var exception = Record.Exception(() => new KafkaInitTableArgs<object, object>(state));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "KafkaInitTableArgs can be created with valid state.")]
+        [Trait("Category", "Unit")]
+        public void CanCreateKafkaInitTableArgs()
+        {
+            // Arrange
+            var state = ImmutableDictionary<object, object>.Empty;
+
+            // Act
+            var args = new KafkaInitTableArgs<object, object>(state);
+
+            // Assert
+            args.State.Should().BeSameAs(state);
+        }
+    }
+}
