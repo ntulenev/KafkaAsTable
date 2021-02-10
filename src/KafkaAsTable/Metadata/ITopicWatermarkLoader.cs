34d8decb8123a5b4e5d3fc65ccebccd3693e071e
Nikita Tulenev <n1tulenev@gmail.com>
2021-02-10 09:03:19 +0300

    Add ITopicWatermarkLoader doc

--- src/KafkaAsTable/Metadata/ITopicWatermarkLoader.cs
+++ src/KafkaAsTable/Metadata/ITopicWatermarkLoader.cs
@@ -8,8 +8,18 @@ using KafkaAsTable.Watermarks;
 
 namespace KafkaAsTable.Metadata
 {
+    /// <summary>
+    /// Interface for service that loads <see cref="TopicWatermark"/>.
+    /// </summary>
     public interface ITopicWatermarkLoader
     {
+        /// <summary>
+        /// Loads <see cref="TopicWatermark"/> from Kafka.
+        /// </summary>
+        /// <typeparam name="Key">Message key.</typeparam>
+        /// <typeparam name="Value">Message value.</typeparam>
+        /// <param name="consumerFactory">Factory delegate for creating consumer.</param>
+        /// <param name="ct">Cancellation token.</param>
         public Task<TopicWatermark> LoadWatermarksAsync<Key, Value>(Func<IConsumer<Key, Value>> consumerFactory, CancellationToken ct);
     }
 }
