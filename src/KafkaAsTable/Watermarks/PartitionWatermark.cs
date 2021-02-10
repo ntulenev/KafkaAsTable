eedbb6aefff2dcd5df17154eadb774bfda747c0e
Nikita Tulenev <n1tulenev@gmail.com>
2021-02-10 08:58:28 +0300

    Add partitionwatermark doc

--- src/KafkaAsTable/Watermarks/PartitionWatermark.cs
+++ src/KafkaAsTable/Watermarks/PartitionWatermark.cs
@@ -6,8 +6,18 @@ using KafkaAsTable.Helpers;
 
 namespace KafkaAsTable.Watermarks
 {
+
+    /// <summary>
+    /// Offset watermark for single partition in topic.
+    /// </summary>
     public class PartitionWatermark
     {
+        /// <summary>
+        /// Creates partition offset watermark.
+        /// </summary>
+        /// <param name="topicName">Name of the topic.</param>
+        /// <param name="offset">Raw kafka offset representation.</param>
+        /// <param name="partition">Raw kafka partition representation.</param>
         public PartitionWatermark(string topicName,
                                   WatermarkOffsets offset,
                                   Partition partition)
@@ -26,8 +36,17 @@ namespace KafkaAsTable.Watermarks
             _partition = partition;
         }
 
+        /// <summary>
+        /// Checks that partition if valid for reading.
+        /// </summary>
         public bool IsReadyToRead() => _offset.High > _offset.Low;
 
+        /// <summary>
+        /// Checks that end of the partition is achieved by consumer.
+        /// </summary>
+        /// <typeparam name="K">Message key.</typeparam>
+        /// <typeparam name="V">Message value.</typeparam>
+        /// <param name="consumeResult">Consumer result.</param>
         public bool IsWatermarkAchievedBy<K, V>(ConsumeResult<K, V> consumeResult)
         {
             if (consumeResult is null)
@@ -38,9 +57,18 @@ namespace KafkaAsTable.Watermarks
             return consumeResult.Offset != _offset.High - 1;
         }
 
+        /// <summary>
+        /// Creates single-partition topic for assigning with current offset.
+        /// </summary>
         public TopicPartitionOffset CreateTopicPartitionWithHighOffset() =>
             new TopicPartitionOffset(new TopicPartition(_topicName, _partition), _offset.High);
 
+        /// <summary>
+        /// Assing consumer to a partition as topic.
+        /// </summary>
+        /// <typeparam name="K">Message key.</typeparam>
+        /// <typeparam name="V">Message value.</typeparam>
+        /// <param name="consumer">Consumer.</param>
         public void AssingWithConsumer<K,V>(IConsumer<K, V> consumer)
         {
             if (consumer is null)
