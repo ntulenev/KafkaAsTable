35efcf57c474c0e771136a966ee2601b94a60e28
Nikita Tulenev <n1tulenev@gmail.com>
2021-02-14 10:14:20 +0300

    Add CouldAssingConsumerForTopicWatermark Test

--- src/KafkaAsTable.Tests/TopicWatermarkTests.cs
+++ src/KafkaAsTable.Tests/TopicWatermarkTests.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 using Confluent.Kafka;
 
 using FluentAssertions;
 
+using KafkaAsTable.Metadata;
 using KafkaAsTable.Watermarks;
 
 using Moq;
@@ -64,26 +67,39 @@ namespace KafkaAsTable.Tests
             exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
         }
 
-        //[Fact(DisplayName = "Consumer could be assigned for TopicWatermark.")]
-        //[Trait("Category", "Unit")]
-        //public void CouldAssingConsumerForTopicWatermark()
-        //{
+        [Fact(DisplayName = "Consumer could be assigned for TopicWatermark.")]
+        [Trait("Category", "Unit")]
+        public void CouldAssingConsumerForTopicWatermark()
+        {
 
-        //    // Arrange
-        //    var partitionWatermarks = (new Mock<IEnumerable<PartitionWatermark>>()).Object;
-        //    var watermark = new TopicWatermark(partitionWatermarks);
-        //    var consumerMock = new Mock<IConsumer<object, object>>();
-        //    var consumer = consumerMock.Object;
+            // Arrange
+            var topicName = new TopicName("Test");
+            var offset = new WatermarkOffsets(new Offset(1), new Offset(2));
+            var partition = new Partition(1);
+            var watermark = new PartitionWatermark(topicName, offset, partition);
 
-        //    // Act
-        //    var exception = Record.Exception(() => watermark.AssignWithConsumer(consumer));
+            var topicWatermark = new TopicWatermark(new[] { watermark });
+            var consumerMock = new Mock<IConsumer<object, object>>();
+            var consumer = consumerMock.Object;
 
-        //    // Assert
-        //    exception.Should().BeNull();
+            // Act
+            var exception = Record.Exception(() => topicWatermark.AssignWithConsumer(consumer));
 
-        //    //Add setup and mock for
-        //    //Watermarks.Select
-        //    //watermark.CreateTopicPartitionWithHighOffset() 
-        //}
+            // Assert
+            exception.Should().BeNull();
+
+            consumerMock.Verify(x => x.Assign(It.Is<IEnumerable<TopicPartitionOffset>>(value =>
+                        value.Single().Topic == topicName.Value
+                        &&
+                        value.Single().Partition == partition
+                        &&
+                        value.Single().Offset == offset.High
+                        &&
+                        value.Single().TopicPartition.Topic == topicName.Value
+                        &&
+                        value.Single().TopicPartition.Partition == partition
+                        )
+                ), Times.Once);
+        }
     }
 }
